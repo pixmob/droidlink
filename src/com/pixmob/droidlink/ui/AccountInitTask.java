985deb61ce6d98dd7321a055844f88e4cc07356f
Alexandre Roman <alexandre.roman@gmail.com>
2011-09-28 19:57:26 +0200

    Fix device identifier generation

--- src/com/pixmob/droidlink/ui/AccountInitTask.java
+++ src/com/pixmob/droidlink/ui/AccountInitTask.java
@@ -81,9 +81,9 @@ class AccountInitTask extends AsyncTask<String, Void, Integer> {
         final String oldAccount = prefs.getString(SP_KEY_ACCOUNT, null);
         
         // Make sure this user has an unique device identifier.
-        if (!newAccount.equals(oldAccount)) {
-            prefsEditor.putString(SP_KEY_DEVICE_ID, DeviceUtils.getDeviceId(fragment.getActivity(),
-                newAccount));
+        if (!prefs.contains(SP_KEY_DEVICE_ID) || !newAccount.equals(oldAccount)) {
+            prefsEditor.putString(SP_KEY_DEVICE_ID,
+                DeviceUtils.getDeviceId(fragment.getActivity(), newAccount));
         }
         
         prefsEditor.putString(SP_KEY_ACCOUNT, newAccount);
